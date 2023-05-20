a0ec3dd9413b3538a5e2074af3d25c16df949760
NG YI MIN <122591096+Iyoshin@users.noreply.github.com>
2023-05-20 13:38:30 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,10 +4,12 @@ public class Player {
     private ArrayList<Card> hands = new ArrayList<Card>();
     private ArrayList<Card> playedCards = new ArrayList<Card>();
     private String name;
+    private int score;
 
-    public Player(ArrayList<Card> hands, String  name) {
+    public Player(ArrayList<Card> hands, String name) {
         this.hands = hands;
         this.name = name;
+        this.score = 0;
     }
 
     public void showPlayerCards() {
@@ -21,7 +23,8 @@ public class Player {
     public int compareCard(Card tempCard) {
         int index = -1;
         for (int i = 0; i < hands.size(); i++) {
-            if (tempCard.getRank().equals(hands.get(i).getRank()) && tempCard.getSuit().equals(hands.get(i).getSuit())) {
+            if (tempCard.getRank().equals(hands.get(i).getRank())
+                    && tempCard.getSuit().equals(hands.get(i).getSuit())) {
                 index = i;
                 break;
             }
@@ -30,24 +33,13 @@ public class Player {
         return index;
     }
 
-    // debugging purpose
-    // public void showSpecificCard(int i) {
-    //     System.out.println(hands.get(i));
-    // }
-    // debugging purpose   
-    // public int noOfCardLeft() {
-    //     return hands.size();
-    // }
-
-
     public void playCard(Card tempCard) {
         hands.remove(compareCard(tempCard));
         playedCards.add(tempCard);
     }
 
-
-    public Boolean isValidMove(Card card) {
-        if(compareCard(card) == -1){
+    public Boolean isValidCard(Card card) {
+        if (compareCard(card) == -1) {
             return false;
         } else {
             return true;
@@ -64,6 +56,42 @@ public class Player {
 
     public boolean handIsEmpty() {
         return hands.isEmpty();
+    }
+
+    // REMAINING CARDS
+    public ArrayList<Card> getHands() {
+        return hands;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int calculateScore() {
+        int score = 0;
+        for (Card card : hands) {
+            if (!hands.equals(null)) {
+                int rankValue = card.getRankValue();
+                // ACE, 1
+                if (rankValue == 13) {
+                    score += 1;
+                }
+                // 10,J,Q,K
+                else if (rankValue >= 9) {
+                    score += 10;
+                }
+                // 2-9
+                else {
+                    score += rankValue + 1;
+                }
+            }
+        }
+        setScore(score);
+        return score;
     }
 
 }
